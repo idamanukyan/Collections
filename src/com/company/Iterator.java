4035f66f0ed30e80e61852faaf7bc02dd6de3dfb
idamanukyan <ida_manukyan@edu.aua.am>
2023-04-02 18:51:08 +0400

    added generic Iterator

---
+++ src/com/company/Iterator.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.NoSuchElementException;
+
+public class Iterator<T> {
+
+    private T[] array;
+    private int index;
+    private boolean canRemove;
+
+    public Iterator(T[] array) {
+        this.array = array;
+        this.index = 0;
+        this.canRemove = false;
+    }
+
+    public boolean hasNext() {
+        return index < array.length;
+    }
+
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        T element = array[index];
+        index++;
+        canRemove = true;
+        return element;
+    }
+
+    public void remove() {
+        if (!canRemove) {
+            throw new IllegalStateException();
+        }
+        int removeIndex = index - 1;
+        for (int i = removeIndex; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = null;
+        index--;
+        canRemove = false;
+    }
+}
